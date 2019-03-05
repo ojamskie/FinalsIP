b71be852a7d2d3e3a5323121e82fc0ca10695319
ojamskie <plasabas1820495@mkt.ceu.edu.ph>
2019-03-05 11:29:00 +0800

    Collision

---
+++ Draw.java
@@ -0,0 +1,44 @@
+import javax.swing.JComponent;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+public class Draw extends JComponent{
+
+	private BufferedImage image;
+	private BufferedImage backgroundImage;
+
+	Hero hero1;
+	Monster monster1;
+	Monster monster2;
+	Monster monster3;
+
+	public Draw(){
+		hero1 = new Hero(30,30, this);
+		monster1 = new Monster(200, 200);
+		monster2 = new Monster(300, 200);
+		monster3 = new Monster(400, 200);
+
+		try{
+			backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.YELLOW);
+		g.drawImage(backgroundImage, 0, 0, this);
+	
+		g.drawImage(hero1.image, hero1.x, hero1.y, this);
+		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
+		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
+		g.drawImage(monster3.image, monster3.xPos, monster3.yPos, this);
+	}
+}
---
+++ Monster.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.awt.Rectangle;
+
+public class Monster{
+	
+	public int xPos = 150;
+	public int yPos = 300;
+	public int hp = 10;
+	public int width;
+	public int height;
+
+	public BufferedImage image;
+	public URL resource = getClass().getResource("slime/idle0.png");
+
+	public Monster(){
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public Monster(int xPass, int yPass){
+		xPos = xPass;
+		yPos = yPass;
+
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		width = image.getWidth();
+		height = image.getHeight();
+	}
+
+	public Rectangle monsterCollision(){
+		return(new Rectangle(xPos, yPos, width, height));
+	}
+
+}
